81c15eccabae5dd47b22d4fb0a408a3e74ac5220
Viktor <khatibvictor97@gmail.com>
2023-09-13 20:58:19 +0400

    feat: Add insertion-sort-list task

---
+++ leetcode/147_insertion-sort-list/insertion-sort-list.go
@@ -0,0 +1,32 @@
+package _47_insertion_sort_list
+
+// Task: https://leetcode.com/problems/insertion-sort-list/
+
+// Solution: https://leetcode.com/problems/insertion-sort-list/submissions/1048564135/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func insertionSortList(head *ListNode) *ListNode {
+	dummy := new(ListNode)
+
+	curr := head
+	for curr != nil {
+		prev := dummy
+
+		for prev.Next != nil && prev.Next.Val <= curr.Val {
+			prev = prev.Next
+		}
+
+		next := curr.Next
+
+		curr.Next = prev.Next
+		prev.Next = curr
+
+		curr = next
+	}
+
+	return dummy.Next
+}
