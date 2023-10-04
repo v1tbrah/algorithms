357029175020c7a8aa04095925d5479b3a347148
Viktor <khatibvictor97@gmail.com>
2023-10-05 00:45:48 +0400

    feat: Update 'isomorphic-strings' task

--- leetcode/205_isomorphic-strings/isomorphic-strings.go
+++ leetcode/205_isomorphic-strings/isomorphic-strings.go
@@ -2,36 +2,35 @@ package isomorphic_strings
 
 // Task: https://leetcode.com/problems/isomorphic-strings/
 
-// Solution: https://leetcode.com/problems/isomorphic-strings/submissions/995736693/
-// Thanks for this explanation: https://leetcode.com/problems/isomorphic-strings/solutions/1332570/my-solution-in-go-with-explanation/
+// Solution: https://leetcode.com/problems/isomorphic-strings/submissions/1067169271/
 
 // tags:
 // #Hash Table
 // #String
 
+// Time: O(n)
+// Space: O(n)
 func isIsomorphic(s string, t string) bool {
-	// s -> t
-	byteMap := make(map[byte]byte)
-
-	// t, who already used for someone s -> t
-	alreadyMapped := make(map[byte]struct{})
-
+	mapped := make(map[byte]byte)
+	usedB := make(map[byte]bool)
 	for i := 0; i < len(s); i++ {
-		b, ok := byteMap[s[i]]
-		if ok && b == t[i] { // if it's exists relation
-			continue
-		}
+		a := s[i]
+		b := t[i]
 
-		if ok && b != t[i] { // if relation s -> t exists, but t[i] != t from this relation
+		mappedA, alreadyMapped := mapped[a]
+		if !alreadyMapped {
+			mapped[a] = b
+			if usedB[b] {
+				// we need map, but it already used in 't'
+				// example: [e: a, a: d], 'a' already used
+				return false
+			}
+			usedB[b] = true
+		} else if mappedA != b {
+			// we already mapped, but not for this char
+			// example: [e: a, e: d], 'e' already mapped to 'a', we can't swap it to 'd'
 			return false
 		}
-
-		if _, ok = alreadyMapped[t[i]]; ok { // if t[i] already mapped, but not for s[i]
-			return false
-		}
-
-		byteMap[s[i]] = t[i]
-		alreadyMapped[t[i]] = struct{}{}
 	}
 
 	return true
